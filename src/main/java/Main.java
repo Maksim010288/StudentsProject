87695a1a74d111d44fd4a1ddcb0c07520d617cca
Maksim <mysik.bazuk@gmail.com>
2022-06-06 07:10:57 +0300

    refactoring class StudentMapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 import db.*;
+import mapper.StudentsMapper;
 
 public class Main {
     public static void main(String[] args) {
-        DbSettingsProvider dbSettingsProvider = new DbSettingsProvider( "src/main/resources/connection.properties");
+        DbSettingsProvider dbSettingsProvider = new DbSettingsProvider("src/main/resources/connection.properties");
         DBSettings dbSettings = dbSettingsProvider.getDBSettings();
         SQLConnector connector = new SQLConnector(dbSettings);
         StudentsDAO requests = new StudentsDAO(connector.getConnection());
         StudentsMapper studentsMapper = new StudentsMapper();
-        requests.studentsAllReturn(studentsMapper);
-        requests.nameByStudentReturn(studentsMapper,"Andriy");
+        requests.returnAll(studentsMapper.map(requests.getResultSet()));
+        requests.returnName("Andriy");
+//        requests.addStudent("Serhiy", "Romaniyk", 3);
     }
 }
--- src/main/java/db/StudentsDAO.java
+++ src/main/java/db/StudentsDAO.java
@@ -1,10 +1,12 @@
 package db;
 
+import mapper.StudentsMapper;
+
 import java.sql.*;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class StudentsDAO {
 
@@ -14,21 +16,23 @@ public class StudentsDAO {
         this.connection = connection;
     }
 
-    private ResultSet getResultSet() throws Exception {
+    public ResultSet getResultSet() {
+        try {
         String out = "select * from students;";
         Statement statement = connection.createStatement();
         return statement.executeQuery(out);
-    }
-
-    public void studentsAllReturn(StudentsMapper studentsMapper) {
-        try {
-            List<Students> students = studentsMapper.resultSetStudent(getResultSet());
-            students.forEach(System.out::println);
-        } catch (Exception e) {
-            e.printStackTrace();
+    }catch (Exception e){
+        e.printStackTrace();
         }
+        return null;
     }
 
+    public void returnAll(List<Students> students) {
+            students.stream()
+                    .sorted(Comparator.comparing(Students::getCourse))
+                    .collect(Collectors.toList())
+                    .forEach(System.out::println);
+    }
 
     private ResultSet getResultSet(String name) throws SQLException {
         String studentName = "select * from students where last_name = ? ";
@@ -38,9 +42,23 @@ public class StudentsDAO {
         return studentStatement.executeQuery();
     }
 
-    public void nameByStudentReturn(StudentsMapper studentsMapper, String name) {
+    public List<String> getByForName(ResultSet resultSet) {
+        List<String> studentList = new ArrayList<>();
+        try (ResultSet setStudent = resultSet){
+            while (setStudent.next()) {
+                int id = setStudent.getInt(1);
+                String student = setStudent.getString(2);
+                studentList.add(id + " " + student);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return studentList;
+    }
+
+    public void returnName(String name) {
         try {
-            List<String> strings = studentsMapper.findAStudentByForName(getResultSet(name));
+            List<String> strings = getByForName(getResultSet(name));
             strings.forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
